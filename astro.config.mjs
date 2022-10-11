aa0aea57eff4709c7faa00597dbd43b97e194977
joevtap <joelvitortorres@gmail.com>
2022-10-10 23:08:48 -0300

    chore: :lipstick: add tailwind configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://engsoft-unifei.github.io',
-  base: '/psimodels'
+  base: '/psimodels',
+  integrations: [tailwind()]
 });
