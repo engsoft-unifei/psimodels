fd3a315cc84f46ee88a422d6b604a7e2c48ba88c
joevtap <joelvitortorres@gmail.com>
2022-10-12 19:37:40 -0300

    feat(ui): :sparkles: add basic herosection and theme switching

---
+++ src/components/ThemeSwitcher.svelte
@@ -0,0 +1,30 @@
+<script>
+  import sun from "/sun.svg";
+  import moon from "/moon.svg";
+  import { onMount } from "svelte";
+
+  let currentTheme = "lofi";
+  let rootTag;
+
+  onMount(() => {
+    rootTag = window.document.documentElement;
+  });
+
+  function switchTheme() {
+    if (currentTheme === "lofi") {
+      currentTheme = "night";
+      rootTag.setAttribute("data-theme", "night");
+    } else {
+      currentTheme = "lofi";
+      rootTag.setAttribute("data-theme", "lofi");
+    }
+  }
+</script>
+
+<button on:click={switchTheme} class="btn btn-circle btn-ghost">
+  {#if currentTheme === "lofi"}
+    <img src={sun} alt="icone tema claro" />
+  {:else}
+    <img src={moon} alt="icone tema escuro" />
+  {/if}
+</button>
